bef6e5a25d1f399e42f7e011be85db116438b0fc
Christopher Koenig <koenig97@stanford.edu>
2019-12-11 16:18:52 -0800

    Add code for identifying misclassifications.

--- evaluate.py
+++ evaluate.py
@@ -4,6 +4,8 @@ Script to evaluate saved sklearn models that have been trained on the LeafSnap i
 
 import argparse
 
+import numpy as np
+
 import data
 import models
 import utils
@@ -17,17 +19,19 @@ def parse_args():
                         help='Feature extract used to produce image embeddings')
     parser.add_argument('-m', '--model_name', type=str, required=True,
                         help='Name of model to evaluate')
-    parser.add_argument('-n', '--top_n', type=int, default=5,
+    parser.add_argument('-n', '--top_n', type=int,
                         help='N for top-N accuracy')
     parser.add_argument('-t', '--is_test', default=False, action="store_true",
                         help='Evaluate on test split.')
+    parser.add_argument('-e', '--errors', default=False, action="store_true",
+                        help='Calculate where model makes misclassifications.')                        
     parser.add_argument('-c', '--confusion_matrix', default=False, action="store_true",
                         help='Create confusion matrix from predictions.')
-
+    
     return parser.parse_args()
 
 
-def eval_saved_model(feature_extractor, model_name, n, is_test, cm=False):
+def eval_saved_model(feature_extractor, model_name, n, is_test, make_cm=False):
     print('Loading data...')
     X_train, X_val, X_test, y_train, y_val, y_test = data.load_data(args.feature_extractor)
     X, y = (X_test, y_test) if is_test else (X_val, y_val)
@@ -36,12 +40,48 @@ def eval_saved_model(feature_extractor, model_name, n, is_test, cm=False):
     model = models.load_model(model_name)
     model_type = model_name[:3] # will be svm or mlp
     predictions = models.top_1_accuracy(model, X, y)
-    models.top_n_accuracy(model, X, y, n, model_type)
+    if args.top_n:
+        models.top_n_accuracy(model, X, y, n, model_type)
 
-    if cm:
+    if args.errors:
+        print('Finding misclassifications...')
+        cm = utils.calculate_cm(predictions, y)
+        cm = utils.normalize_cm(cm)
+        misclassifications(cm)
+    if make_cm:
         print('Creating confusion matrix...')
         cm_path = 'images/cm_' + model_name + '.png'
-        utils.make_cm(predictions, y, cm_path)
+        utils.plot_cm(predictions, y, cm_path)
+
+
+def misclassifications(cm):
+    species_info = []
+    for i in range(cm.shape[0]):
+        accuracy = cm[i][i]
+
+        # top 6 predicted labels for species i
+        labels = np.argsort(cm[i])[-6:]
+        
+        # exclude species i itself from the list
+        labels = [l for l in labels if l != i][-5:]
+
+        # percentage of misclassifications of species i
+        # that are due to predicting species l instead
+        percentages = [cm[i][l] for l in labels]
+
+        species_info.append((accuracy, labels, percentages))
+    
+    print('Leaf Species Classification Accuracies:')
+    accuracies = [s[0] for s in species_info]
+    idxs = np.argsort(accuracies)
+    for idx in idxs:
+        print('Accuracy for Species {0}: {1:.2f}'.format(utils.labels_to_species[idx], accuracies[idx]))
+        labels = species_info[idx][1]
+        percentages = species_info[idx][2]
+        print('Misclassified as:')
+        for i in range(len(labels)):
+            print('Species {0}: {1:.2f}'.format(utils.labels_to_species[labels[i]], percentages[i]))
+        print()
 
 
 def main(args):
--- utils.py
+++ utils.py
@@ -9,22 +9,226 @@ from sklearn.metrics import confusion_matrix
 
 # Mapping from labels to species for the LeafSnap image embeddings
 labels_to_species = {
-    # TODO: Fill in once mapping of label numbers to species names is known
+    0: 'abies_concolor',
+    1: 'abies_nordmanniana',
+    2: 'acer_campestre',
+    3: 'acer_ginnala',
+    4: 'acer_griseum',
+    5: 'acer_negundo',
+    6: 'acer_palmatum',
+    7: 'acer_pensylvanicum',
+    8: 'acer_platanoides',
+    9: 'acer_pseudoplatanus',
+    10: 'acer_rubrum',
+    11: 'acer_saccharinum',
+    12: 'acer_saccharum',
+    13: 'aesculus_flava',
+    14: 'aesculus_glabra',
+    15: 'aesculus_hippocastamon',
+    16: 'aesculus_pavi',
+    17: 'ailanthus_altissima',
+    18: 'albizia_julibrissin',
+    19: 'amelanchier_arborea',
+    20: 'amelanchier_canadensis',
+    21: 'amelanchier_laevis',
+    22: 'asimina_triloba',
+    23: 'betula_alleghaniensis',
+    24: 'betula_jacqemontii',
+    25: 'betula_lenta',
+    26: 'betula_nigra',
+    27: 'betula_populifolia',
+    28: 'broussonettia_papyrifera',
+    29: 'carpinus_betulus',
+    30: 'carpinus_caroliniana',
+    31: 'carya_cordiformis',
+    32: 'carya_glabra',
+    33: 'carya_ovata',
+    34: 'carya_tomentosa',
+    35: 'castanea_dentata',
+    36: 'catalpa_bignonioides',
+    37: 'catalpa_speciosa',
+    38: 'cedrus_atlantica',
+    39: 'cedrus_deodara',
+    40: 'cedrus_libani',
+    41: 'celtis_occidentalis',
+    42: 'celtis_tenuifolia',
+    43: 'cercidiphyllum_japonicum',
+    44: 'cercis_canadensis',
+    45: 'chamaecyparis_pisifera',
+    46: 'chamaecyparis_thyoides',
+    47: 'chionanthus_retusus',
+    48: 'chionanthus_virginicus',
+    49: 'cladrastis_lutea',
+    50: 'cornus_florida',
+    51: 'cornus_kousa',
+    52: 'cornus_mas',
+    53: 'corylus_colurna',
+    54: 'crataegus_crus-galli',
+    55: 'crataegus_laevigata',
+    56: 'crataegus_phaenopyrum',
+    57: 'crataegus_pruinosa',
+    58: 'crataegus_viridis',
+    59: 'cryptomeria_japonica',
+    60: 'diospyros_virginiana',
+    61: 'eucommia_ulmoides',
+    62: 'evodia_daniellii',
+    63: 'fagus_grandifolia',
+    64: 'ficus_carica',
+    65: 'fraxinus_americana',
+    66: 'fraxinus_nigra',
+    67: 'fraxinus_pennsylvanica',
+    68: 'ginkgo_biloba',
+    69: 'gleditsia_triacanthos',
+    70: 'gymnocladus_dioicus',
+    71: 'halesia_tetraptera',
+    72: 'ilex_opaca',
+    73: 'juglans_cinerea',
+    74: 'juglans_nigra',
+    75: 'juniperus_virginiana',
+    76: 'koelreuteria_paniculata',
+    77: 'larix_decidua',
+    78: 'liquidambar_styraciflua',
+    79: 'liriodendron_tulipifera',
+    80: 'maclura_pomifera',
+    81: 'magnolia_acuminata',
+    82: 'magnolia_denudata',
+    83: 'magnolia_grandiflora',
+    84: 'magnolia_macrophylla',
+    85: 'magnolia_soulangiana',
+    86: 'magnolia_stellata',
+    87: 'magnolia_tripetala',
+    88: 'magnolia_virginiana',
+    89: 'malus_angustifolia',
+    90: 'malus_baccata',
+    91: 'malus_coronaria',
+    92: 'malus_floribunda',
+    93: 'malus_hupehensis',
+    94: 'malus_pumila',
+    95: 'metasequoia_glyptostroboides',
+    96: 'morus_alba',
+    97: 'morus_rubra',
+    98: 'nyssa_sylvatica',
+    99: 'ostrya_virginiana',
+    100: 'oxydendrum_arboreum',
+    101: 'paulownia_tomentosa',
+    102: 'phellodendron_amurense',
+    103: 'picea_abies',
+    104: 'picea_orientalis',
+    105: 'picea_pungens',
+    106: 'pinus_bungeana',
+    107: 'pinus_cembra',
+    108: 'pinus_densiflora',
+    109: 'pinus_echinata',
+    110: 'pinus_flexilis',
+    111: 'pinus_koraiensis',
+    112: 'pinus_nigra',
+    113: 'pinus_parviflora',
+    114: 'pinus_peucea',
+    115: 'pinus_pungens',
+    116: 'pinus_resinosa',
+    117: 'pinus_rigida',
+    118: 'pinus_strobus',
+    119: 'pinus_sylvestris',
+    120: 'pinus_taeda',
+    121: 'pinus_thunbergii',
+    122: 'pinus_virginiana',
+    123: 'pinus_wallichiana',
+    124: 'platanus_acerifolia',
+    125: 'platanus_occidentalis',
+    126: 'populus_deltoides',
+    127: 'populus_grandidentata',
+    128: 'populus_tremuloides',
+    129: 'prunus_pensylvanica',
+    130: 'prunus_sargentii',
+    131: 'prunus_serotina',
+    132: 'prunus_serrulata',
+    133: 'prunus_subhirtella',
+    134: 'prunus_virginiana',
+    135: 'prunus_yedoensis',
+    136: 'pseudolarix_amabilis',
+    137: 'ptelea_trifoliata',
+    138: 'pyrus_calleryana',
+    139: 'quercus_acutissima',
+    140: 'quercus_alba',
+    141: 'quercus_bicolor',
+    142: 'quercus_cerris',
+    143: 'quercus_coccinea',
+    144: 'quercus_falcata',
+    145: 'quercus_imbricaria',
+    146: 'quercus_macrocarpa',
+    147: 'quercus_marilandica',
+    148: 'quercus_michauxii',
+    149: 'quercus_montana',
+    150: 'quercus_muehlenbergii',
+    151: 'quercus_nigra',
+    152: 'quercus_palustris',
+    153: 'quercus_phellos',
+    154: 'quercus_robur',
+    155: 'quercus_rubra',
+    156: 'quercus_shumardii',
+    157: 'quercus_stellata',
+    158: 'quercus_velutina',
+    159: 'quercus_virginiana',
+    160: 'robinia_pseudo-acacia',
+    161: 'salix_babylonica',
+    162: 'salix_caroliniana',
+    163: 'salix_matsudana',
+    164: 'salix_nigra',
+    165: 'sassafras_albidum',
+    166: 'staphylea_trifolia',
+    167: 'stewartia_pseudocamellia',
+    168: 'styrax_japonica',
+    169: 'styrax_obassia',
+    170: 'syringa_reticulata',
+    171: 'taxodium_distichum',
+    172: 'tilia_americana',
+    173: 'tilia_cordata',
+    174: 'tilia_europaea',
+    175: 'tilia_tomentosa',
+    176: 'toona_sinensis',
+    177: 'tsuga_canadensis',
+    178: 'ulmus_americana',
+    179: 'ulmus_glabra',
+    180: 'ulmus_parvifolia',
+    181: 'ulmus_procera',
+    182: 'ulmus_pumila',
+    183: 'ulmus_rubra',
+    184: 'zelkova_serrata',
 }
 
 images_dir = 'images/'
 
 
-def make_cm(predictions, labels, path):
+def plot_cm(predictions, labels, path):
     """
-    Calculate confusion matrix given predictions and labels and plot.
+    Plot confusion matrix.
+
+    :param - predictions: list for which index i holds the prediction for embedding i
+    :param - labels: list for which index i holds ground truth label for embedding i
+    :param - path: path to which to save the plotted confusion matrix
+    """
+    cm = calculate_cm(predictions, labels)
+    cm = normalize_cm(cm)
+    plot_heatmap(cm, path, 'Predicted Label', 'True Label')
+
+
+def calculate_cm(predictions, labels):
+    """
+    Calculate confusion matrix.
 
     :param - predictions: list for which index i holds the prediction for embedding i
     :param - labels: list for which index i holds ground truth label for embedding i
     """
-    cm = confusion_matrix(labels, predictions)
-    cm = cm / np.sum(cm, axis=1)[:, None] # Normalize each row to sum to 1
-    plot_heatmap(cm, path, 'Predicted Label', 'True Label')
+    return confusion_matrix(labels, predictions)
+
+
+def normalize_cm(confusion_matrix):
+    """
+    Row-normalize a confusion matrix such that each row sums to 1.
+
+    :param - confusion_matrix: 2D numpy array representing a confusion matrix.
+    """
+    return confusion_matrix / np.sum(confusion_matrix, axis=1)[:, None]
 
 
 def plot_heatmap(matrix, path, xlabel=None, ylabel=None):
